f98cf3148dda2bccca208db2f2c2b0ae9194f271
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-02-18 14:12:35 +0800

    feat(services): redesign services section with enhanced UI/UX

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import Techstack from "./components/Techstack";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-950">
